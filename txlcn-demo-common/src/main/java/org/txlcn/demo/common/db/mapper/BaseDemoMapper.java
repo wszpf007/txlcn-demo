3d67d4b57134d2c616c3fd6f837500468ce0e301
月月鸟 <438371426@qq.com>
2019-07-22 09:45:06 +0800

    提交增加eureka的pom文件

--- txlcn-demo-common/src/main/java/org/txlcn/demo/common/db/mapper/BaseDemoMapper.java
+++ txlcn-demo-common/src/main/java/org/txlcn/demo/common/db/mapper/BaseDemoMapper.java
@@ -1,9 +1,10 @@
 package org.txlcn.demo.common.db.mapper;
 
-import org.txlcn.demo.common.db.domain.Demo;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+import org.txlcn.demo.common.db.domain.Demo;
 
 /**
  * Description:
@@ -16,7 +17,12 @@ public interface BaseDemoMapper {
 
     @Insert("insert into t_demo(kid, demo_field, group_id, create_time,app_name) values(#{kid}, #{demoField}, #{groupId}, #{createTime},#{appName})")
     void save(Demo demo);
-
+    
+    
+    @Update("update t_demo set demo_field=#{demoField},group_id=#{groupId},create_time=#{createTime},app_name=#{appName} where kid = #{kid}")
+    void update(Demo demo);
+    
+    
     @Delete("delete from t_demo where id=#{id}")
     void deleteByKId(Long id);
 }
